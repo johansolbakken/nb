c6f2c0af145aaba30c4f6dd103a97e9e5bfb372b
Johan Solbakken <johsol@stud.ntnu.no>
2023-05-26 19:37:39 +0200

    If in cfg

--- src/cfg.rs
+++ src/cfg.rs
@@ -12,19 +12,13 @@ pub enum Opcode {
     Sub,
     Mul,
     Div,
-    Cmp,
-    Jmp,
-    Jz,
-    Jnz,
-    Jl,
-    Jle,
-    Jg,
-    Jge,
+    CmpEq,
     Call,
     Ret,
     Push,
     Print,
     Set,
+    If,
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -299,12 +293,123 @@ impl CFG {
                                 self.blocks.push(block);
                                 seq_id = id;
                             }
+                            lexer::TokenType::Symbol(symbol) => {
+                                let id = self.next_id();
+                                let temp_id = self.next_temp_id();
+                                let instruction = Instruction {
+                                    id,
+                                    opcode: Opcode::Set,
+                                    operands: vec![
+                                        Operand::Temporary(temp_id),
+                                        Operand::Variable(symbol.clone()),
+                                    ],
+                                };
+                                let block = BasicBlock {
+                                    id,
+                                    instructions: vec![instruction],
+                                    predecessors: vec![seq_id],
+                                    successors: vec![],
+                                };
+                                self.blocks.push(block);
+                                seq_id = id;
+                            }
                             _ => {
                                 panic!("No token found for expression {:?}", token);
                             }
                         }
                     } else {
                         panic!("No token found for expression");
+                    }
+                }
+            }
+            ast::NodeType::IfStatement => {
+                // empty start block
+                let empty_start_id = self.next_id();
+                let block = BasicBlock {
+                    id: empty_start_id,
+                    instructions: Vec::new(),
+                    predecessors: vec![seq_id],
+                    successors: vec![],
+                };
+                self.blocks.push(block);
+
+                // generate condition
+                let condition = &ast.children[0];
+                seq_id = self.create_basic_blocks(condition, empty_start_id);
+                let condition_temp = self.get_last_temp_id();
+                let condition_id = seq_id;
+
+                // Generate if
+                let if_instruction = Instruction {
+                    id: self.next_id(),
+                    opcode: Opcode::If,
+                    operands: vec![Operand::Temporary(condition_temp)],
+                };
+                let if_id = self.next_id();
+                let if_block = BasicBlock {
+                    id: if_id,
+                    instructions: vec![if_instruction],
+                    predecessors: vec![condition_id],
+                    successors: vec![],
+                };
+                self.blocks.push(if_block);
+
+                // generate if block
+                let body = &ast.children[1];
+                seq_id = self.create_basic_blocks(body, if_id);
+                let body_id = seq_id;
+
+                let mut else_block_id = if_id;
+                if ast.children.len() == 3 {
+                    // generate else block
+                    let else_block = &ast.children[2];
+                    seq_id = self.create_basic_blocks(else_block, if_id);
+                    else_block_id = seq_id;
+                }
+
+                let end_id = self.next_id();
+                let end_block = BasicBlock {
+                    id: end_id,
+                    instructions: Vec::new(),
+                    predecessors: vec![body_id, else_block_id],
+                    successors: vec![],
+                };
+                self.blocks.push(end_block);
+                seq_id = end_id;
+            }
+            ast::NodeType::Condition => {
+                let expr1 = &ast.children[0];
+                seq_id = self.create_basic_blocks(expr1, seq_id);
+                let expr1_temp = self.get_last_temp_id();
+
+                let expr2 = &ast.children[1];
+                seq_id = self.create_basic_blocks(expr2, seq_id);
+                let expr2_temp = self.get_last_temp_id();
+
+                match ast.data.as_str() {
+                    "==" => {
+                        let id = self.next_id();
+                        let temp_id = self.next_temp_id();
+                        let instruction = Instruction {
+                            id,
+                            opcode: Opcode::CmpEq,
+                            operands: vec![
+                                Operand::Temporary(temp_id),
+                                Operand::Temporary(expr1_temp),
+                                Operand::Temporary(expr2_temp),
+                            ],
+                        };
+                        let block = BasicBlock {
+                            id,
+                            instructions: vec![instruction],
+                            predecessors: vec![seq_id],
+                            successors: vec![],
+                        };
+                        self.blocks.push(block);
+                        seq_id = id;
+                    }
+                    _ => {
+                        panic!("Unknown operator");
                     }
                 }
             }
@@ -332,7 +437,7 @@ impl CFG {
     fn add_empty_exit_block(&mut self) {
         let id = self.next_id();
         self.exit = id;
-        let predecessor = self.blocks.len() - 1;
+        let predecessor = self.blocks[self.blocks.len() - 1].id;
         let block = BasicBlock {
             id,
             instructions: Vec::new(),
